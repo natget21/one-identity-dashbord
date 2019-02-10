763f0a35b2b105d73d71d9a604a62cf8d45c03d9
Natnael Getachew <natget21@gmail.com>
2019-02-10 09:14:19 +0300

    account registeration fixed

--- src/views/Base/pages/AddAdmin.js
+++ src/views/Base/pages/AddAdmin.js
@@ -59,156 +59,102 @@ class Forms extends Component {
                     </Col>
                   </FormGroup> */}
         <Row>
-          <Col xs="12" md = "6" >
+          <Col md={{ size: 10, offset: 1 }}>
             <Card>
               <CardHeader>
-                <strong>Personal Information </strong> 
+                <strong>Register New Account </strong> 
               </CardHeader>
               <CardBody>
-                <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
+                <Form action="" method="post" className="form-horizontal">
                   
                   <FormGroup row>
-                    <Col md="3">
+                    <Col md={{ size: 2, offset: 2 }}>
                       <Label htmlFor="text-input">First Name</Label>
                     </Col>
-                    <Col md="9">
+                    <Col md="6">
                       <Input type="text" id="text-input" name="text-input" placeholder="Enter First Name" />
                      
                     </Col>
                   </FormGroup>
                   <FormGroup row>
-                    <Col md="3">
+                    <Col md={{ size: 2, offset: 2 }}>
                       <Label htmlFor="text-input">Middle Name</Label>
                     </Col>
-                    <Col md="9">
+                    <Col md="6">
                       <Input type="text" id="text-input" name="text-input" placeholder="Enter Middle Name" />
                      
                     </Col>
                   </FormGroup>
                   <FormGroup row>
-                    <Col md="3">
+                    <Col md={{ size: 2, offset: 2 }}>
                       <Label htmlFor="text-input">Last Name</Label>
                     </Col>
-                    <Col md="9">
+                    <Col md="6">
                       <Input type="text" id="text-input" name="text-input" placeholder="Enter Last Name" />
                      
                     </Col>
                   </FormGroup>
                   <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="date-input">Date of Birth </Label>
+                    <Col md={{ size: 2, offset: 2 }}>
+                      <Label htmlFor="text-input">Email</Label>
                     </Col>
-                    <Col md="9">
-                      <Input type="date" id="date-input" name="date-input" placeholder="date" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label>Gender</Label>
-                    </Col>
-                    <Col md="9">
-                      <FormGroup check inline>
-                        <Input className="form-check-input" type="radio" id="inline-radio1" name="inline-radios" value="option1" />
-                        <Label className="form-check-label" check htmlFor="inline-radio1">Female</Label>
-                      </FormGroup>
-                      <FormGroup check inline>
-                        <Input className="form-check-input" type="radio" id="inline-radio2" name="inline-radios" value="option2" />
-                        <Label className="form-check-label" check htmlFor="inline-radio2">Male</Label>
-                      </FormGroup>
-                      
-                    </Col>
-                  </FormGroup>
+                    <Col md="6">
+                      <Input type="text" id="text-input" name="text-input" placeholder="Enter Email Address" />
 
-                  <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="text-input">Citizenship</Label>
                     </Col>
-                    <Col md="9">
-                      <Input type="text" id="text-input" name="text-input" placeholder="Enter Citizenship" />
-                  
-                    </Col>
-                  </FormGroup>
-
-                 
-                  
-                </Form>
-              </CardBody>
-              {/* <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter> */}
-            </Card>
-            <Card>
-              <CardHeader>
-                <strong>Inline</strong> Form
-              </CardHeader>
-              <CardBody>
-                <Form action="" method="post" inline>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputName2" className="pr-1">Name</Label>
-                    <Input type="text" id="exampleInputName2" placeholder="Jane Doe" required />
-                  </FormGroup>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputEmail2" className="pr-1">Email</Label>
-                    <Input type="email" id="exampleInputEmail2" placeholder="jane.doe@example.com" required />
                   </FormGroup>
                 </Form>
               </CardBody>
               <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+                <center>
+                  <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Register</Button>
+                  <Button type="reset" size="sm" color="danger" style={{ marginLeft: "20px" }}><i className="fa fa-ban"></i> Reset</Button>
+                </center>
               </CardFooter>
-            </Card>
-          </Col>
-          <Col xs="12" md = "6" >
-            
-            
-            <Card style = {{ "height" : "60%"}}>
-              <CardHeader>
-                <strong> Picture Input</strong> 
-              </CardHeader>
-              <CardBody >
-                <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
-                  
-                <FormGroup row>
-                    <Col md="3">
-                      <Label htmlFor="file-input">pic</Label>
-                    </Col>
-                    <Col xs="12" md="9">
-                      <Input type="file" id="file-input" name="file-input" />
-                    </Col>
-                  </FormGroup>
-                </Form>
-              </CardBody>              
             </Card>
 
-            <Card>
-              <CardHeader>
-                <strong>Inline</strong> Form
-              </CardHeader>
-              <CardBody>
-                <Form action="" method="post" inline>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputName2" className="pr-1">Name</Label>
-                    <Input type="text" id="exampleInputName2" placeholder="Jane Doe" required />
-                  </FormGroup>
-                  <FormGroup className="pr-1">
-                    <Label htmlFor="exampleInputEmail2" className="pr-1">Email</Label>
-                    <Input type="email" id="exampleInputEmail2" placeholder="jane.doe@example.com" required />
-                  </FormGroup>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-                <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-              </CardFooter>
-            </Card>
           </Col>
-          <Col xs="12" md="6">
-         </Col>
         </Row>
         
-        
+        <Row>
+          <Col md={{ size: 10, offset: 1 }}>
+            <Card>
+              <CardHeader>
+                <strong>Add Company To Whitlist </strong>
+              </CardHeader>
+              <CardBody>
+                <Form action="" method="post" className="form-horizontal">
+
+                  <FormGroup row>
+                    <Col md={{ size: 2, offset: 2 }}>
+                      <Label htmlFor="text-input">Company Name</Label>
+                    </Col>
+                    <Col md="6">
+                      <Input type="text" id="text-input" name="text-input" placeholder="Enter First Name" />
+
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md={{ size: 2, offset: 2 }}>
+                      <Label htmlFor="text-input">Key</Label>
+                    </Col>
+                    <Col md="6">
+                      <Input type="text" id="text-input" name="text-input" placeholder="Key" />
+
+                    </Col>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <CardFooter>
+                <center>
+                  <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Add To Whitlist</Button>
+                  <Button type="reset" size="sm" color="danger" style={{ marginLeft: "20px" }}><i className="fa fa-ban"></i> Reset</Button>
+                </center>
+              </CardFooter>
+            </Card>
+
+          </Col>
+        </Row>
         
         
       </div>
